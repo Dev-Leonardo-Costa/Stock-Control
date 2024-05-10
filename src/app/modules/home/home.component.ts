118bf81f9ed82e1afe587b05c46182ad7ecba1b9
Leonardo Costa <devleonardo27@gmail.com>
2024-05-09 22:46:44 -0300

    feat: completando o form do projeto com as ação

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
-  
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signUp = this.formBuilder.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('lOGADO COM SUCESSO', this.loginForm.value);
+  }
+
+  createAcountLoginForm(): void {
+    console.log('create acount sucessful', this.signUp.value);
+  }
 }
